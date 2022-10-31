fee69a5121c378bbd7d9fa3921ff14aaeaf51e2b
clin445 <94992357+clin445@users.noreply.github.com>
2022-10-31 17:47:14 -0400

    Update config.js
    added keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'QGkNJIHneHw4SPN4lTiN4E1VB',
+  consumer_secret:      'MTQG0rDBNtjIXiVvpy4sn2ecTmjqsqQmKkaxToGmC7xq9MsmKs',
+  access_token:         '1587189141657165825-kyXch6sPP4Ma4zxPPWoHCgBLf1hGUp',
+  access_token_secret:  'oz7rXV6FgdaHDz5Gi0rUXwktujUwA7tTLPR5Uc8cZIFjF'
 }
