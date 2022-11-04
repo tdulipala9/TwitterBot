10372209a28cbe6de4c972d207748cf3b597fc5c
clin445 <94992357+clin445@users.noreply.github.com>
2022-11-04 17:53:23 -0400

    runbot

--- bot.js
+++ bot.js
@@ -143,26 +143,6 @@ function retweetPopular() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Wordnik stuff
 function nounUrl(minCorpusCount, limit) {
 	return "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun&minCorpusCount=" + minCorpusCount + "&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=" + limit + "&api_key=" + WordnikAPIKey;
@@ -283,96 +263,40 @@ function replyTo(sn, tId) {
     });
 }
 
-function followAMentioner() {
-	T.get('statuses/mentions_timeline', { count:50, include_rts:1 },  function (err, reply) {
-		  if (err !== null) {
-			console.log('Error: ', err);
-		  }
-		  else {
-		  	var sn = reply.pick().user.screen_name;
-			if (debug) 
-				console.log(sn);
-			else {
-				//Now follow that user
-				T.post('friendships/create', {screen_name: sn }, function (err, reply) {
-					if (err !== null) {
-						console.log('Error: ', err);
-					}
-					else {
-						console.log('Followed: ' + sn);
-					}
-				});
-			}
-		}
-	});
-}
-
-function respondToMention() {
-	T.get('statuses/mentions_timeline', { count:100, include_rts:0 },  function (err, reply) {
-		  if (err !== null) {
-			console.log('Error: ', err);
-		  }
-		  else {
-		  	mention = reply.pick();
-		  	mentionId = mention.id_str;
-		  	mentioner = '@' + mention.user.screen_name;
-		  	
-		  	var tweet = mentioner + " " + pre.pick();
-			if (debug) 
-				console.log(tweet);
-			else
-				T.post('statuses/update', {status: tweet, in_reply_to_status_id: mentionId }, function(err, reply) {
-					if (err !== null) {
-						console.log('Error: ', err);
-					}
-					else {
-						console.log('Tweeted: ', tweet);
-					}
-				});
-		  }
-	});
-}
 
 function runBot() {
-	console.log(" "); // just for legible logs
-	var d=new Date();
-	var ds = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-	console.log(ds);  // date/time of the request	
+  
+    var d=new Date();
+    var ds = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+    console.log("Run Bot at: ", ds);  // date/time of the request   
 
-	// Get 200 nouns with minimum corpus count of 5,000 (lower numbers = more common words) 
-	request(nounUrl(5000,200), function(err, response, data) {
-		if (err != null) return;		// bail if no data
-		nouns = eval(data);
 
-		// Filter out the bad nouns via the wordfilter
-		
-		for (var i = 0; i < nouns.length; i++) {
-			if (wordfilter.blacklisted(nouns[i].word))
-			{
-				console.log("Blacklisted: " + nouns[i].word);
-				nouns.remove(nouns[i]);
-				i--;
-			}				
-		}
 
-		
-		
-		///----- NOW DO THE BOT STUFF
-		var rand = Math.random();
+    ///----- NOW DO THE BOT STUFF
+    //1. post a status and like it
+    //2. find one i am interested in, retweet it, and like it
+    //3. if someone mention me, reply the message and follow the user
+    //4. pick one message randomly to like
+    var rand = Math.random();
 
- 		if(rand <= 1.60) {      
-			console.log("-------Tweet something");
-			tweet();
-			
-		} else if (rand <= 0.80) {
-			console.log("-------Tweet something @someone");
-			respondToMention();
-			
-		} else {
-			console.log("-------Follow someone who @-mentioned us");
-			followAMentioner();
-		}
-	});
+    console.log('rand value: ', rand);
+    if(rand <= 0.30) {      
+        console.log("-------Tweet something");
+        tweet();
+        
+    } else if (rand <= 0.60) {
+        console.log("-------find mentions and reply to @someone");
+        //respondToMention();
+        replyMentions();
+        
+    } else if(rand <= 0.8) {
+        console.log("-------find the latest interested message, retweet it and like it");
+        retweetLatest();
+    }
+    else {
+        console.log("-------find the latest random message to like");
+        randLike();
+    }
 }
 	/*//post gif preparations
 	{
